d560f86a26ab4b08884e1dc2b8bfd224286073f2
heqian <664516715@qq.com>
2018-11-15 13:36:14 +0800

    backup

---
+++ src/components/home/homemain/HomeMain.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="nb-home-main">
+    <div class="nkn-currency">
+      <ul>
+        <li>
+          <currency-plugin title="Balance" data="18000.0000" unit="NKN"></currency-plugin>
+        </li>
+        <li>
+          <currency-plugin title="Mining Reward" data="20000.0000" unit="NKN"></currency-plugin>
+        </li>
+        <li>
+          <currency-plugin title="NKN/USDT Price" data="0.05100" unit="USDT"></currency-plugin>
+        </li>
+        <li>
+          <currency-plugin title="NKN/ETH Price" data="0.0002210" unit="ETH"></currency-plugin>
+        </li>
+      </ul>
+    </div>
+    <div class="nkn-block">
+      <ul>
+        <li class="current-height"></li>
+        <li class="current-transection"></li>
+        <li class="average-time"></li>
+        <li class="total-node"></li>
+        <li class="version-info"></li>
+      </ul>
+    </div>
+    <div class="nkn-node"></div>
+  </div>
+</template>
+
+<script>
+import CurrencyPlugin from '@/components/home/plugins/CurrencyPlugin.vue'
+export default {
+  name:'home-main',
+  components: {
+    CurrencyPlugin
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.nb-home-main{
+  width: 928px;
+  ul{
+    margin: 0;padding: 0;
+    display: inline-block;
+    li{
+      list-style-type: none;
+      float: left;
+    }
+  }
+  .nkn-currency{
+    ul{
+      li{
+        width: 220px;height: 90px;
+      }
+      li+li{
+        margin-left: 16px;
+      }
+    }
+  }
+  .nkn-block{
+    margin-top: 16px;
+    ul{
+
+    }
+  }
+}
+</style>
---
+++ src/components/home/plugins/CurrencyPlugin.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="currency-plugin">
+    <div class="currency-title">{{title}}</div>
+    <div class="currency-content clearfix">
+      <div class="currency-data">{{data}}</div>
+      <div class="currency-unit">{{unit}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    data: String,
+    unit: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.currency-plugin{
+  background: @common-bg-color;
+  .mixin-boxshadow;
+  padding: 20px;
+  .currency-title{
+    font-size: 14px;
+    color:black;
+    line-height: 1;
+  }
+  .currency-content{
+    margin-top: 16px;
+    .currency-data{
+      font-size: 20px;
+      color: @si-color;
+      float: left;
+      line-height: 1;
+    }
+    .currency-unit{
+      font-size: 14px;
+      color:black;
+      float: right;
+      margin-top: 6px;
+      line-height: 1;
+    }
+  }
+}
+</style>
