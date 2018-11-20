fd6e77277fcfb3db84b4b7fa0c9c28daf25dbaa0
heqian <664516715@qq.com>
2018-11-20 20:19:52 +0800

    backup

--- src/components/home/plugins/CurrencyPlugin.vue
+++ src/components/home/plugins/CurrencyPlugin.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="currency-plugin">
-  <div class="currency-header clearfix">
+  <div class="currency-header">
     <div class="currency-title">{{title}}</div>
     <button v-if="copy" class="currency-copy" v-clipboard:copy="data">
       <img src="../../../assets/img/icon/copy.png">
       <span>copy</span>
     </button>
   </div>
-  <div class="currency-content clearfix">
+  <div class="currency-content">
     <div class="currency-data">{{data}}</div>
       <div v-if="unit" class="currency-unit">{{unit}}</div>
     </div>
@@ -31,17 +31,18 @@ export default {
   .mixin-boxshadow;
 
   .currency-header {
-    height: 12px;
+    .home-common-flex;
+    align-items: center;
 
     .currency-title {
-      float: left;
       .home-common-title-font;
     }
 
     .currency-copy {
-      float: right;
       cursor: pointer;
-      outline: none;background:none;border:none;
+      outline: none;
+      background: none;
+      border: none;
 
       img {
         width: 10px;
@@ -59,16 +60,16 @@ export default {
   .currency-content {
     margin-top: 16px;
     line-height: 20px;
+    .home-common-flex;
+    align-items: center;
 
     .currency-data {
       .home-commom-data-font;
-      float: left;
       word-break: break-all;
     }
 
     .currency-unit {
       .home-common-title-font;
-      float: right;
       margin-top: 6px;
     }
   }
