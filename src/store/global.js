bb421c2f7ce7d7b56c5405e29bcfa4ddcea0d851
heqian <664516715@qq.com>
2018-11-20 19:17:53 +0800

    backup

--- src/store/global.js
+++ src/store/global.js
@@ -14,5 +14,10 @@ export default {
       if (state.userInfo) return state.userInfo
       return window.sessionStorage.getItem('account')
     }
+  },
+  mutation: {
+    setLoading (state, value) {
+      state.loading = value
+    }
   }
 }
